cf9ed44c0fa010f8e1120b4969633b7f2cfa7154
DUNNSGCOM\agaspari <agaspari@dunnsolutions.com>
2017-06-19 15:39:03 -0500

    Added password form.

---
+++ src/com/core/NewPasswordFrame.java
@@ -0,0 +1,84 @@
+package com.core;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by agaspari on 6/19/2017.
+ */
+public class NewPasswordFrame {
+    private GridPane frame;
+    private Label lblPassword, lblRepeatPassword, lblError;
+    private PasswordField txtPassword, txtRepeatPassword;
+    private Button btnSet;
+    private Encrypter encrypter;
+    private File password;
+    public NewPasswordFrame(){
+        encrypter = new Encrypter();
+        frame = new GridPane();
+        frame.setVgap(5);
+        frame.setHgap(5);
+        frame.setPadding(new Insets(5));
+        lblPassword = new Label("Password: ");
+        lblRepeatPassword = new Label("Repeat Password: ");
+        txtPassword = new PasswordField();
+        txtRepeatPassword = new PasswordField();
+        btnSet = new Button("Set");
+        btnSet.setMinWidth(100);
+        lblError = new Label("Error: Test");
+        lblError.setMinWidth(200);
+        lblError.setAlignment(Pos.CENTER);
+        lblError.setTextFill(Color.web("#800000"));
+        lblError.setStyle("-fx-border-color: #800000; -fx-background-color: rgba(128, 0, 0, .3)");
+        lblError.setFont(Font.font("Tahoma", FontWeight.BOLD, 12));
+        lblError.setVisible(false);
+        btnSet.setOnAction(
+            e ->{
+                if(!txtPassword.getText().equals(txtRepeatPassword.getText())){
+                    lblError.setText("Error: Passwords don't match.");
+                    lblError.setVisible(true);
+                }else if(txtPassword.getText().length() < 6){
+                    lblError.setText("Error: Password is too short.");
+                    lblError.setVisible(true);
+                }else{
+                    try {
+                        password = new File("password.wver");
+                        password.createNewFile();
+                        FileWriter fw = new FileWriter(password);
+                        fw.write(encrypter.encryptText(txtPassword.getText()));
+                        fw.close();
+                        txtPassword.setText("");
+                        txtRepeatPassword.setText("");
+                        lblError.setVisible(false);
+                        Main.getPrimaryStage().setScene(new Scene(MainFrame.getFrame(), 300, 275));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                frame.setVisible(false);
+            }
+        );
+
+        frame.add(lblPassword, 0, 0);
+        frame.add(lblRepeatPassword, 0, 1);
+        frame.add(txtPassword, 1, 0);
+        frame.add(txtRepeatPassword, 1 ,1);
+        frame.add(btnSet, 1, 2);
+        frame.add(lblError, 0, 3, 3, 1);
+    }
+
+    public GridPane getFrame(){
+        return frame;
+    }
+}
---
+++ src/com/core/PasswordFrame.java
@@ -0,0 +1,77 @@
+package com.core;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by agaspari on 6/19/2017.
+ */
+public class PasswordFrame {
+    private GridPane frame;
+    private PasswordField txtPassword;
+    private Button btnSubmit;
+    private Label lblPassword, lblError;
+    private Encrypter encrypter;
+    private File password;
+    public PasswordFrame(){
+        encrypter = new Encrypter();
+        password = new File("password.wver");
+        frame = new GridPane();
+        frame.setVgap(5);
+        frame.setHgap(5);
+        frame.setPadding(new Insets(5));
+        lblPassword = new Label("Password: ");
+        txtPassword = new PasswordField();
+        btnSubmit = new Button("Submit");
+        lblError = new Label("Error: Test");
+        lblError.setMinWidth(200);
+        lblError.setAlignment(Pos.CENTER);
+        lblError.setTextFill(Color.web("#800000"));
+        lblError.setStyle("-fx-border-color: #800000; -fx-background-color: rgba(128, 0, 0, .3)");
+        lblError.setFont(Font.font("Tahoma", FontWeight.BOLD, 12));
+        lblError.setVisible(false);
+        btnSubmit.setOnAction(
+            e -> {
+                try {
+                    if(encrypter.decryptText(readFile(password.getPath(), Charset.defaultCharset())).equals(txtPassword.getText())){
+                        Main.getPrimaryStage().setScene(new Scene(MainFrame.getFrame(), 300, 275));
+                        lblError.setVisible(false);
+                    }else{
+                        lblError.setText("Error: Invalid Password.");
+                        lblError.setVisible(true);
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        );
+
+        frame.add(lblPassword, 0, 0);
+        frame.add(txtPassword, 1, 0);
+        frame.add(btnSubmit, 0, 1);
+        frame.add(lblError, 0, 2, 3, 1);
+    }
+
+    public GridPane getFrame(){
+        return frame;
+    }
+
+    private String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+}
